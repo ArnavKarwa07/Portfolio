3cf1613bff4178bbd057e704be5f5c7bbcf8f85f
ArnavKarwa07 <arnavkarwa07@gmail.com>
2025-02-22 15:55:23 +0530

    router issue fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+//import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+// import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} /> */}
       </Routes>
       <Footer />
     </Router>
